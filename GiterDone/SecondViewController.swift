9a4a374866d890a2060e7ae3b7b5b4978a5b272f
John Hawley <jehawley@gmail.com>
2018-08-27 20:51:56 -0700

    Initial Commit

---
+++ GiterDone/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GiterDone
+//
+//  Created by John Hawley on 8/27/18.
+//  Copyright © 2018 John Hawley. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
